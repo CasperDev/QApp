b75d20b22ec6d8819a364a3d9d1dd994a3d5baa7
CasperDev <casperdev@4ab.pl>
2026-04-17 15:36:20 +0200

    confirmation gitignore works

--- App/main.cpp
+++ App/main.cpp
@@ -8,8 +8,8 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     Libcore core;
-    QString title = "Calc: 2+3=%1";
-    w.setWindowTitle(title.arg(core.add(2,3)));
+    QString title = "Calc: 3+4=%1";
+    w.setWindowTitle(title.arg(core.add(3,4)));
     w.show();
     return QCoreApplication::exec();
 }
